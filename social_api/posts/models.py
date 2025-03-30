6935f4f156315180e742734f43c4f1d1d14055a3
Farouq Taiwo <Farouqtaiwo45@gmail.com>
2025-03-30 20:23:11 +0100

    post app modified

--- social_api/posts/models.py
+++ social_api/posts/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User  # Import default User model
 
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
