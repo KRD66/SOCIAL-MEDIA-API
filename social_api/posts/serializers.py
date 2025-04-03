f44a0ec2ef136fd5efb22484825850b5a9c7aafa
Farouq Taiwo <Farouqtaiwo45@gmail.com>
2025-04-03 17:37:08 +0100

    profile serializers added

--- social_api/posts/serializers.py
+++ social_api/posts/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-from .models import Post, Comment
+from .models import Post, Comment, Profile
+from django.contrib.auth.models import User
 
 class PostSerializer(serializers.ModelSerializer):
       likes_count =   serializers.IntegerField(source='likes.count', read_only=True)
@@ -11,3 +12,13 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = '__all__'
+        
+        
+class ProfileSerializer(serializers.ModelSerializer):
+    followers_count = serializers.IntegerField(source='followers.count', read_only=True)
+    following_count = serializers.IntegerField(source='user.following.count', read_only=True)
+
+    class Meta:
+        model = Profile
+        fields = ['user', 'followers_count', 'following_count']
+
